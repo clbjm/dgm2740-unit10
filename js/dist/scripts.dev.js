0723b99aaabe0775391436ac49520f5b24bbc09f
jonathon mullins <jonathonm93@gmail.com>
2020-11-12 16:41:50 -0700

    first commit

---
+++ js/dist/scripts.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function toggleMenu() {
+  document.getElementById("hambergerBtn").classList.toggle("open");
+  document.getElementById("primaryNav").classList.toggle("open");
+}
+
+var x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
+//# sourceMappingURL=scripts.dev.js.map
